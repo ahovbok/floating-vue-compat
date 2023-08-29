4bd75bfc4c9872527b34008b3f5d9dda7f5b9505
Aleksey Khovbok <65286299+ahovbok@users.noreply.github.com>
2023-08-29 11:15:42 +0200

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import PopperWrapper from './PopperWrapper.vue'
+
+const Component = ({
+  ...PopperWrapper,
+  name: 'VMenu',
+  vPopperTheme: 'menu',
+}) as unknown as typeof PopperWrapper
+
+export default Component
+</script>
